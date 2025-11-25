2511e40982543add01543bf92404e7920233bf06
Patrick Desgrange <pdesgrange@gmail.com>
2025-11-25 08:41:42 -0500

    correcting mispelling

--- score_parser.py
+++ score_parser.py
@@ -22,13 +22,13 @@ def parse_scores_csv(scores_uploaded_file):
         pass
     else:
         # If the file has extra rows before headers, reload with skiprows
-        uploaded_file.seek(0)  # reset file pointer
+        scores_uploaded_file.seek(0)  # reset file pointer
         # Read all lines
-        lines = uploaded_file.readlines()
+        lines = scores_uploaded_file.readlines()
         # Find index of line starting with "match"
         start_idx = next(i for i, line in enumerate(lines) if line.decode().startswith("Match"))
         # Reload DataFrame from that line onward
-        uploaded_file.seek(0)
+        scores_uploaded_file.seek(0)
         df_scores = pd.read_csv(pd.compat.StringIO("".join([l.decode() for l in lines[start_idx:]])))
     
     # Create uniq_id with total first, then shots
